30aa7ddeb1c6fde065706b89d54eb97dc31ae22e
Siddhant Shekhar <shekharsidhant88@gmail.com>
2026-08-24 16:42:56 +0530

    string length modified

--- Phase1/stringLength.js
+++ Phase1/stringLength.js
@@ -1,7 +1,7 @@
 const stringLength = (str) => {
   let count = 0;
 
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 0; str[i] !== undefined; i++) {
     count++;
   }
   console.log(`The length of the string is ${count}`);
