96c7c2f1c9b52de129368cfa87aed6d148712e32
Siddhant Shekhar <shekharsidhant88@gmail.com>
2026-08-24 23:52:43 +0530

    little change compare string

--- Phase1/stringContainsDigits.js
+++ Phase1/stringContainsDigits.js
@@ -2,15 +2,14 @@ const stringContainsDigits = (str) => {
   for (let i = 0; i < str.length; i++) {
     let code = str.charCodeAt(i);
 
-    if (code >= 48 && code <= 57) {
-      return true;
-    } else {
-        return false;
+    if (code < 48 || code > 57) {
+      return false;
     }
   }
-  return false;
+  return true;
 };
 
-console.log(stringContainsDigits("hello123"));
+console.log(stringContainsDigits("123hello123"));
 console.log(stringContainsDigits("hello"));
 console.log(stringContainsDigits("123"));
+console.log(stringContainsDigits("hello123"));
