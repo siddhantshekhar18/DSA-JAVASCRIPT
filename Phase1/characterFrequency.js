172d66d59cb94bad6772979521b945bdf023ad25
Siddhant Shekhar <shekharsidhant88@gmail.com>
2026-08-26 11:55:13 +0530

    update: added charcter frequency count

---
+++ Phase1/characterFrequency.js
@@ -0,0 +1,14 @@
+const characterFrequency = (str) => {
+  let frequency = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (frequency[str[i]]) {
+      frequency[str[i]]++;
+    } else {
+      frequency[str[i]] = 1;
+    }
+  }
+  return frequency;
+};
+
+console.log(characterFrequency("Programmingp"));
